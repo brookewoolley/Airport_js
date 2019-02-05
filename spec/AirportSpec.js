32ac6e9bd7812e9a2ac5f9b62a634ca281b309ba
Brooke Woolley <brooke.woolley@gmail.com>
2019-02-05 17:13:57 +0000

    Implemented ability for plane to land at airport

---
+++ spec/AirportSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('Airport', function(){
+  var airport;
+  var plane;
+  beforeEach(function(){
+    airport = new Airport();
+  });
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+  it('can clear planes for landing', function(){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
+  });
+});
---
+++ spec/FeatureSpec.js
@@ -0,0 +1,15 @@
+describe("Airport", function() {
+  let plane;
+  let airport;
+
+  beforeEach(function(){
+    plane = new Plane();
+    airport = new Airport();
+  });
+  
+  it('planes can be instructed to land at an airport', function() {
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+
+});
